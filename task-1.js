1bb67a20e58af6e7de8e61c6bec2a30a5babd27a
chakki1234 <chakkirala.a@gmail.com>
2019-05-24 23:32:54 +0530

    almost final

--- task-1.js
+++ task-1.js
@@ -12,19 +12,22 @@ var list1=document.querySelectorAll(".list1");
 var list2=document.querySelectorAll(".list2");
 var list3=document.querySelectorAll(".list3");
 var list4=document.querySelectorAll(".list4");
+var retreive=document.querySelector("#retreive");
+var deleteall=document.querySelector("#deleteall");
+
 
 var liitem1=null;
 var liitem2=null;
 var liitem3=null;
 var liitem4=null;
-
+var arr= new Array;
 var calreqval=0;
 var calreqval2=0;
 var ageval=0;
 var weightval=0;
 var heightval=0;
 var delcount=0;
-
+var valueno2=0;
 var listno1=-1;
 var listno2=0;
 var listno3=0;
@@ -33,6 +36,7 @@ var listno4=0;
 function calcal(){
   calreqval2=calreqval2-liitem4;
   calreq.innerHTML="<br><br><h2>"+calreqval2 +" Calories more to go</h2>" ;
+  localStorage.setItem("calreqval",calreqval2);
 
   if(calreqval2<50&&calreqval2>30)
   {
@@ -51,6 +55,22 @@ function calcal(){
   }
 }
 
+retreive.addEventListener("click",function(){
+  valueno2=Number(localStorage.getItem("valueno"));
+  
+  console.log(valueno2);
+
+  for(var k=0;k<valueno2;k++)
+  {
+  	list1[k].innerHTML=localStorage.getItem("food["+k+"]");
+    list2[k].innerHTML=localStorage.getItem("Carbs["+k+"]");
+    list3[k].innerHTML=localStorage.getItem("Fat["+k+"]");
+    list4[k].innerHTML=localStorage.getItem("Calorie["+k+"]");
+  }
+  listno1=valueno2-1;
+  calreqval2=localStorage.getItem("calreqval");
+});
+
 del.addEventListener("click", function(){
 if(listno1>=-1)
 {
@@ -61,8 +81,10 @@ if(listno1>=-1)
     list2[listno1].innerHTML="";
     list3[listno1].innerHTML="";
     list4[listno1].innerHTML="";
+    calreqval2=calreqval2+arr[listno1];
+    calreq.innerHTML="<br><br><h2>"+calreqval2 +" Calories more to go</h2>" ;
     listno1=listno1-1;
-
+    localStorage.setItem("valueno",listno1+1);
 }
 
 if(delcount===0)
@@ -71,8 +93,11 @@ list1[listno1].innerHTML="";
 list2[listno1].innerHTML="";
 list3[listno1].innerHTML="";
 list4[listno1].innerHTML="";
+calreqval2=calreqval2+arr[listno1];
+calreq.innerHTML="<br><br><h2>"+calreqval2 +" Calories more to go</h2>" ;
 delcount=delcount+1;
 listno1=listno1-1;
+localStorage.setItem("valueno",listno1+1);
 /*listno1=listno1-1;*/
 }
 
@@ -84,7 +109,6 @@ listno1=listno1-1;
 
 age.addEventListener("keyup", function(event)
 {   
-	/*if(event.which===13)*/
 		ageval=this.value;
        
 	
@@ -92,7 +116,6 @@ age.addEventListener("keyup", function(event)
 
 weight.addEventListener("keyup", function(event)
 {   
-	/*if(event.which===13)*/
 		weightval=this.value;
 		
 
@@ -100,7 +123,6 @@ weight.addEventListener("keyup", function(event)
 
 height.addEventListener("keyup", function(event)
 {   
-	/*if(event.which===13)*/
 		heightval=this.value;
 	
 
@@ -110,7 +132,7 @@ click.addEventListener("click" ,function(){
 
 calreqval=(10*weightval)+(6.25*heightval)-(5*ageval)+5;
 calreqval2=calreqval;
-calreq.innerHTML="<br><br><br><h1>CAlROIE REQUIREMENT  "+ calreqval;
+calreq.innerHTML="<br><br><br><h2>CAlROIE REQUIREMENT  "+ calreqval+"</h2>";
 
 });
 
@@ -119,121 +141,53 @@ add1.addEventListener("click" , function()
   delcount=0;	
   ++listno1;
   liitem1=prompt("Enter your food items");
-  list1[listno1].innerHTML=liitem1;
 
-  if(liitem1=="samosa")
-  {
-   list1[listno1].innerHTML=liitem1;
-   list2[listno1].innerHTML="5";/*listno1++;*/
-   list3[listno1].innerHTML="13";
-   list4[listno1].innerHTML="260";
-   liitem4=260;
-   calcal();
-  
+   if(liitem1==null)
+   	listno1=listno1-1;
+
+  else
+  {	
+  	list1[listno1].innerHTML=liitem1;
+  	localStorage.setItem("food["+listno1+"]",liitem1);
+   	localStorage.setItem("valueno",listno1+1);
   }
-
-  if(liitem1=="naan")
-  {
-   list1[listno1].innerHTML=liitem1;
-   list2[listno1].innerHTML="11.5";/*listno1++;*/
-   list3[listno1].innerHTML="2.1";
-   list4[listno1].innerHTML="78";
-   liitem4=78;
-   calcal();
-  }
-
-  if(liitem1=="palak paneer")
-  {
-   list1[listno1].innerHTML=liitem1;
-   list2[listno1].innerHTML="43";/*listno1++;*/
-   list3[listno1].innerHTML="14";
-   list4[listno1].innerHTML="350";
-   liitem4=350;
-   calcal();
-
-  }
-
-  if(liitem1=="daal")
-  {
-   list1[listno1].innerHTML=liitem1;
-   list2[listno1].innerHTML="24";/*listno1++;*/
-   list3[listno1].innerHTML="12";
-   list4[listno1].innerHTML="108";
-   liitem4=108;
-   calcal();
-  }
-
-  if(liitem1=="kurma")
-  {
-   list1[listno1].innerHTML=liitem1;
-   list2[listno1].innerHTML="52";/*listno1++;*/
-   list3[listno1].innerHTML="9";
-   list4[listno1].innerHTML="330";
-   liitem4=330;
-   calcal();
-  }
-
-  if(liitem1=="bhaji")
-  {
-   list1[listno1].innerHTML=liitem1;
-   list2[listno1].innerHTML="57";/*listno1++;*/
-   list3[listno1].innerHTML="16";
-   list4[listno1].innerHTML="190";
-   liitem4=190;  
-   calcal();
-  }
-
-  if(liitem1=="briyani")
-  {
-   list1[listno1].innerHTML=liitem1;
-   list2[listno1].innerHTML="59";/*listno1++;*/
-   list3[listno1].innerHTML="25";
-   list4[listno1].innerHTML="550";
-   liitem4=550;
-   calcal();
-  }
-
-})
+});
 
 add2.addEventListener("click" , function()
 { 
- 
-  console.log("working");
  
   if(list1[listno1].innerHTML!="")
   {
   	liitem2=prompt("Enter Carbs Content(in g)");
     list2[listno1].innerHTML=liitem2;
-   /* listno2++;*/
+    localStorage.setItem("Carbs["+listno1+"]",liitem2); 
+ 
   }
 
-})
+});
+
 add3.addEventListener("click" , function()
 { 
   if(list1[listno1].innerHTML!="")
  {
   liitem3=prompt("Enter Fat Content(in g)");
   list3[listno1].innerHTML=liitem3;
-  /*listno3++;*/
+  localStorage.setItem("Fat["+listno1+"]",liitem3); 
 }
 
-})
+});
+
 add4.addEventListener("click" , function()
 { 
 if(list1[listno1].innerHTML!="")
  {
-  liitem4=prompt("Enter Calorie Content");
-
+  liitem4=Number(prompt("Enter Calorie Content"));
   list4[listno1].innerHTML=liitem4;
+  localStorage.setItem("Calorie["+listno1+"]",liitem4); 
+  arr[listno1]=liitem4;
   calcal();
-  /*listno4++;*/
  }
 
- 
-  
-
- 
-
-})
+ });
 
 
